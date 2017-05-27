fd3b5a9b03ea7f933dc9d059af0b9433d0f2c4f5
力力娃 <liliwa@192.168.0.104>
2017-05-28 00:27:58 +0800

    完成前端注册页面  登录注册盒子的切换 注册提交 开始后端数据的验证和保存

---
+++ public/js/index.js
@@ -0,0 +1,41 @@
+$(function() {
+
+    var $loginBox = $('#loginBox');
+    var $registerBox = $('#registerBox');
+    var $userInfo = $('#userInfo');
+
+    //切换到注册面板
+    $loginBox.find('a.colMint').on('click', function() {
+        $registerBox.show();
+        $loginBox.hide();
+    });
+
+    //切换到登录面板
+    $registerBox.find('a.colMint').on('click', function() {
+        $loginBox.show();
+        $registerBox.hide();
+    });
+
+    //注册
+    $registerBox.find('button').on('click',function () {
+        //通过ajax的方式去提交
+        $.ajax({
+            types : 'post',
+            url:'/api/user/register',
+            data: {
+              username:$registerBox.find('[name=username]').val(),
+              password:$registerBox.find('[name=password]').val(),
+              repassword:$registerBox.find('[name=repassword]').val()
+            },
+            dataType: 'json',
+            success:function (result) {
+                console.log(result)
+            }
+        })
+    })
+
+    //登录
+
+    //退出
+
+})
