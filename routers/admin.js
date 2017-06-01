eb0f5f265100751ef87a147321a206a886a8849e
力力娃 <liliwa@192.168.0.104>
2017-06-01 20:39:08 +0800

    注册数据分页原理和实现，开始提取成公共组件

--- routers/admin.js
+++ routers/admin.js
@@ -25,14 +25,52 @@ router.get('/', function (req, res) {
 });
 // 用户管理
 router.get('/user', function (req, res) {
-    //读取一个模板  从数据库中读取所有用户的数据，然后分配给模板，展示  要用到模型
-    User.find().then(function (users) {
-        // console.log(users)
-        res.render('admin/user_index', {
-            userInfo: req.userInfo,
-            users: users
-        })
+    /*
+    * 读取一个模板  从数据库中读取所有用户的数据，然后分配给模板，展示  要用到模型
+    * limit(Number) 限制获取的数据条数
+    * 比如说 每页两条  共4条  第一页  12   第二页  34  我们可以每次读取两条  第二页 从第三条读起 方法skip
+    * skip()  忽略数据的条数
+    * 总结 每页显示两条
+    * 1: 1-2  skip: 0
+    * 2: 3-4  skip: 2
+    * 3: 5-6  skip: 4
+    * 可以看出来  我们的skip是根据我们的页数来的  （当前页-1）*limit
+    * */
+
+    //用户通过http的方式传递一个page过来，有可能不传 默认1
+    var page = Number(req.query.page || 1) ;
+    var limit = 2;
+    //总页数
+    var pages = 0;
+
+    //获取总的条数
+    User.count().then(function (count) {
+        // console.log(count)
+
+        //计算总页数
+        pages = Math.ceil(count/limit);
+        //取值不超过pages
+        page = Math.min(page,pages);
+        //取值不能小于1
+        page= Math.max(page,1);
+
+        var skip = (page-1)*limit;
+
+        User.find().limit(limit).skip(skip).then(function (users) {
+            // console.log(users)
+            res.render('admin/user_index', {
+                userInfo: req.userInfo,
+                users: users,
+
+                page: page,
+                count: count,
+                pages: pages,
+                limit: limit
+            })
+        });
     });
+
+
 });
 
 
