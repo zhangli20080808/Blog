c39bd7777ffcbbeef1c4ed54f4032c55bcaf5536
力力娃 <liliwa@192.168.0.104>
2017-06-01 19:54:24 +0800

    后台管理，用户注册信息的读取和展示

--- routers/admin.js
+++ routers/admin.js
@@ -4,22 +4,36 @@
 
 var express = require('express');
 
+var User = require('../models/users');
+
 //router去匹配我们use定义的一些东西
 var router = express.Router();
 
-router.use(function (req,res,next) {
-    if(!req.userInfo.isAdmin){
+router.use(function (req, res, next) {
+    if (!req.userInfo.isAdmin) {
         //如果是非管理员
         res.send('只有管理员才能进入这个页面')
         return;
     }
     next();
 });
-
-router.get('/',function (req,res) {
-    res.render('admin/index',{
-        userInfo:req.userInfo
+//首页
+router.get('/', function (req, res) {
+    res.render('admin/index', {
+        userInfo: req.userInfo
     })
 });
+// 用户管理
+router.get('/user', function (req, res) {
+    //读取一个模板  从数据库中读取所有用户的数据，然后分配给模板，展示  要用到模型
+    User.find().then(function (users) {
+        // console.log(users)
+        res.render('admin/user_index', {
+            userInfo: req.userInfo,
+            users: users
+        })
+    });
+});
+
 
 module.exports = router;
